9b1436e8fad45fef9e1c7e9cdad81570cbcb0f32
parkphi11ip <angham@naver.com>
2022-12-29 17:44:55 +0900

    data & q.

--- day02/src/data/Test11.java
+++ day02/src/data/Test11.java
@@ -8,13 +8,23 @@ public class Test11 {
 		// q. bmi 계산기 --- 조건 남섬 180cm, 80kg
 		
 		double m = 1.8;
-		double kg = 80;
-		
-		double m_m = m*m;
-				
+		double kg = 80;		
+		double m_m = m*m;			
 		double bmi = kg/m_m;
 		
 		System.out.println(bmi);
+		
+		
+		//소수점 2자리만 살리기
+		double bmi2 = bmi*100;
+		System.out.println(bmi2);
+		int bmi3 = (int)bmi2;
+		System.out.println(bmi3);
+		double bmi4 = bmi3;
+		System.out.println(bmi4);
+		double bmi5 = bmi4/100;
+		System.out.println(bmi5);
+		
 		
 	}
 
---
+++ day02/src/data/Test12.java
@@ -0,0 +1,22 @@
+package data;
+
+public class Test12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 10;
+		double b = a; // int -> double (자동)
+		
+		double c = 1.5;
+		int d = (int)c; // double -> int (강제)
+		
+		System.out.println(b);
+		System.out.println(d);
+		
+		// 어떠한 경우에도 실수 > 정수
+		
+		
+	}
+
+}
