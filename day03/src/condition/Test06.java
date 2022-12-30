e33465c1a5e95a5b9aa26b9c0586fbd2f9ba16ec
phi11ip <angham@naver.com>
2022-12-31 02:56:46 +0900

    과

--- day03/src/condition/Test06.java
+++ day03/src/condition/Test06.java
@@ -3,20 +3,24 @@ package condition;
 public class Test06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		// 들어만 오면 주차요금 1시간은 무조건 5000원
-		// 1시간 이후부터는 10분당 500원
-		// 입력값으로 들어온 시각/분, 나간 시각/분을 설정하고 주차요금을 계산하여 출력
-		
-		
-		
+//		# Test06
+//
+//		주차장 요금 계산 프로그램
+//
+//		KH 주차장은 다음과 같이 주차요금을 계산합니다
+//
+//		- 들어만 오면 1시간은 무조건 기본요금 5000원
+//		- 1시간 이후부터는 10분당 500원으로 계산
+//
+//		입력값으로 들어온 시각/분, 나간 시각/분을 설정하고 주차요금을 계산해서 출력
+			
 		int hfee = 5000;
 		int mfee = 500/10;
+		int Overhfee = 3000;
 		
 		int intimeH = 13, intimeM = 30;		
-		int outtimeH = 13, outtimeM = 50;
-		//1h20m
+		int outtimeH = 13, outtimeM = 58;
 		
 		int prktimeH = outtimeH - intimeH;
 		int prktimeM = outtimeM - intimeM;
@@ -24,16 +28,15 @@ public class Test06 {
 		boolean prkin1hUnder = prktimeH == 0 && prktimeM <= 60;  
 		boolean prkin1hOver = prktimeH >= 1;
 		
-		//boolean finalFee = prkin1hOver && prkin1hUnder;
-		
-		//System.out.println();..
-		
+//		System.out.println(prktimeH);
+//		System.out.println(prktimeM);
 				
 		if(prkin1hUnder) {
-			System.out.println(hfee);
+			System.out.println(hfee+"원입니다.");
 		}
 		else {
-			System.out.println((5000*prktimeH)+(mfee*prktimeM));
+			System.out.println(hfee+(Overhfee*prktimeH-Overhfee)
+					+(mfee*prktimeM)+"원입니다.");
 		}
 				
 		
