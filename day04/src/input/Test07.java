4c235505c96f602d7bdc859c53cb30b0944dcc02
parkphi11ip <angham@naver.com>
2023-01-02 18:20:34 +0900

    Test 07 윤년과제 done.

--- day04/src/input/Test07.java
+++ day04/src/input/Test07.java
@@ -1,44 +1,66 @@
 package input;
 
+import java.util.Scanner;
+
 public class Test07 {
 
 	public static void main(String[] args) {
 
 //		# Test07
 		
-//		이전에 만들었던 KH 여행사 프로모션 프로그램의 기능을 개선할 계획입니다.
+		int dfee = 100000;
 		
-//		- 모든 계절에 따른 할인율은 다음과 같습니다
-//			- 봄 : 10%
-//			- 여름 : 5%
-//			- 가을 : 30%
-//			- 겨울 : 20%
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("여행 갈 년도와 월을 입력하세요");
+		int y = sc.nextInt();
+		int m = sc.nextInt();
 		
-//		- 윤년일 경우 5% 추가 할인을 받습니다
+		System.out.println("여행인원수를 입력하소");
+		int people = sc.nextInt();
 		
-//		사용자에게 이름, 인원수, 예상기간(일), 여행갈 년도와 월 정보를 각각 입력받아 다음과 같은 출력 결과가 나올 수 있도록 구현하세요
-//		> 무조건 1인 1일 10만원 규칙은 그대로 적용됩니다
-//		```
-//		홍길동님의 예상 견적서는 다음과 같습니다
-//		여행 예정 : 2024년 1월
-//		여행 인원수 : 3명
-//		여행 기간 : 10일
+		System.out.println("여행기간 ㄱ");
+		int day = sc.nextInt();
 		
-//		계절 할인 : 10%
-//		계절 할인 : 20%
-//		윤년 할인 : 5%
+		sc.close();
 		
-//		총 금액 : 3000000원
-//		할인 금액 : 450000원
-//		견적 금액 : 2550000원
-//		할인 금액 : 750000원
-//		견적 금액 : 2250000원
+		boolean leapYear = (y % 4 == 0 && y % 100 != 0) || y % 400 == 0;
+		double springRate = 0.9, summerRate = 0.95, fallRate = 0.7, winterRate = 0.8;
+		double leapRate = 0.05;
 		
-//		<버튼을 눌러 예약 사이트로 이동해주세요>
+		int pay = dfee*people*day;
 		
+		if(leapYear) {
+			if(m<=3&&m>=1) {
+				System.out.println(pay*springRate*leapRate);
+			}
+			else if(m>=4&&m<=8) {
+				System.out.println(pay*summerRate*leapRate);
+			}
+			else if(m>=9&&m<=11) {
+				System.out.println(pay*fallRate*leapRate);
+			}
+			else {
+				System.out.println(pay*winterRate*leapRate);
+			}
+		}
 		
+		else{
+			if(m<=3&&m>=1) {
+				System.out.println(pay*springRate);
+			}
+			else if(m>=4&&m<=8) {
+				System.out.println(pay*summerRate);
+			}
+			else if(m>=9&&m<=11) {
+				System.out.println(pay*fallRate);
+			}
+			else {
+				System.out.println(pay*winterRate);
+			}
 		
-		
+		}
+				
 	}
 
 }
