327d1086fd653fe66b8bbebb699d355140e6c3fe
phi11ip <angham@naver.com>
2023-01-03 22:00:24 +0900

    22 01 03

--- day05/src/loop/Test12_plus.java
+++ day05/src/loop/Test12_plus.java
@@ -8,24 +8,24 @@ public class Test12_plus {
 		// TODO Auto-generated method stub
 //		 Test12
 		
-		int cnt = 0;
+		int cnt = 0; 										//cnt 카운터 변수 지정. 
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); 
 		
 		System.out.println(" 몇 단? (9단까지 지원 됩니다.)");
-		int n = sc.nextInt();
+		int n = sc.nextInt(); 								// 인풋변수 지정. 
 		
-		for(int i=n; i<=9; i++) {
+		for(int i=n; i<=9; i++) { 							// n = 인풋 변수 몇단부터 시작인가 지정. 
 			System.out.println( n+"x"+i+"="+"?");
 			System.out.println(" 정답을 입력하세요.");
-			int answer = sc.nextInt();
-			if(answer==i*n) {
+			int answer = sc.nextInt(); 						// 인풋변수 지정 .
+			if(answer==i*n) { 								// 지정한 인풋변수랑 연결 i*n for 반복문의 답
 				System.out.println("------------------");
 				System.out.println(" 정답 ! ");
-				cnt++;
+				cnt++; 										// cnt 변수 : if문 충족시에 1씩 저장.
 				if (cnt ==2) {
-					break;}
-			}else {
+					break;} 								// cnt 변수 : 윗 if문 충족시 1씩저장. 2가 될시에 break를 통해 반복문 끝.
+			}else { 										// 답 틀렸을시에 실행됨. 
 				System.out.println(" 땡 ㅠ ");
 			}
 			
