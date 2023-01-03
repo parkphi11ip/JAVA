8357b46c5624a79c1a13462621df8e09e199feac
parkphi11ip <angham@naver.com>
2023-01-03 12:54:15 +0900

    loop (반복문) 기본예제

---
+++ day05/src/loop/Test01.java
@@ -0,0 +1,18 @@
+package loop;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+
+		//반복
+		// q. hello를 10번 출력하세요
+		
+		// for(i라는 공이 1개 들어가있다; 10개이하면 실행; 1개씩 증가)
+		// for(선언부; 조건부; 증감부)
+		for(int i=1; i<= 10; i++) {
+			System.out.println("hello");	
+		}
+		
+		
+	}
+}
---
+++ day05/src/loop/Test02.java
@@ -0,0 +1,25 @@
+package loop;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+
+		//반복
+		// q. hello를 10번 출력하세요
+		
+		// for(i라는 공이 1개 들어가있다; 10개이하면 실행; 1개씩 증가)
+		// for(선언부; 조건부; 증감부)
+//		for(int i=1; i<= 10; i++) {
+//			System.out.println("hello");	
+//		}
+		
+		// 1부터 10까지 화면에 출력 1,2,3,,,4,,,,7,,10
+		//	반복을 위해 만든 변수를 반목문 내에서 사용할 수 있다.
+		for(int i=0; i<10; i++) {
+			System.out.println(i);
+			
+		}
+		
+		
+	}
+}
