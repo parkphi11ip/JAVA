ec23af821754419c2171d0fead3a0f01a6c65b91
phi11ip <angham@naver.com>
2023-01-03 20:36:59 +0900

    23 01 03 과제 done.

--- day05/src/loop/Test12_plus.java
+++ day05/src/loop/Test12_plus.java
@@ -1,31 +1,37 @@
 package loop;
 
+import java.util.Scanner;
+
 public class Test12_plus {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		 Test12
-//
-//		 사용자가 원하는 구구단을 출력하는 프로그램을 구현하세요
-//
-//		 (+추가) 다 만드셨다면 정답 부분을 사용자가 입력하게 만들어 정답/오답 판정이 가능하게 하세요
-//
-//		 ```java
-//		 //괄호 친 부분은 사용자가 입력하는 부분입니다
-//		 원하는 구구단 : (5)
-//		 5 x 1 = (5)
-//		 정답!
-//		 5 x 2 = (9)
-//		 오답!
-//		 ...
-//		 5 x 9 = (45)
-//		 정답!
-//		 정답 개수 : 2
-//		 ```
 		
-
+		int cnt = 0;
 		
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println(" 몇 단? (9단까지 지원 됩니다.)");
+		int n = sc.nextInt();
 		
+		for(int i=n; i<=9; i++) {
+			System.out.println( n+"x"+i+"="+"?");
+			System.out.println(" 정답을 입력하세요.");
+			int answer = sc.nextInt();
+			if(answer==i*n) {
+				System.out.println("------------------");
+				System.out.println(" 정답 ! ");
+				cnt++;
+				if (cnt ==2) {
+					break;}
+			}else {
+				System.out.println(" 땡 ㅠ ");
+			}
+			
+	}
+		System.out.println("~~~~~~~~~~~~~~~~~~");
+		System.out.println(" 정답개수 : "+cnt);
+		System.out.println("~~~~~~~~~~~~~~~~~~");
 	}
 }
