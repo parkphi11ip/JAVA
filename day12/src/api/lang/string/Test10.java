ed4bcf478853c4145a1039e040ecc0c2b0e28d98
phi11ip <angham@naver.com>
2023-01-14 23:23:04 +0900

    .

--- day12/src/api/lang/string/Test10.java
+++ day12/src/api/lang/string/Test10.java
@@ -1,29 +1,31 @@
 package api.lang.string;
 
 import java.util.Scanner;
-import java.util.regex.Matcher;
+
 import java.util.regex.Pattern;
 
 public class Test10 {
 
 	public static void main(String[] args) {
-		
+		 
 		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("생년월일을 입력하세요 : ");
 
-		System.out.println("주민번호를 입력해주세요. ");
-		String RRN = sc.next();
+		String birth = sc.next();
+
 		sc.close();
 		
-		String regex ="\\d{2}([0]\\d|[1][0-2])([0][1-9]|[1-2]\\d|[3][0-1])[-]*[1-4]\\d{6}";
+		String regex = "^(19|20)[0~9]{2,2}-(0[0-9]|1[0-2])-(0[0-9]|1[0-9]|2[0-9]|3[0-1])$";
 		
-	        Pattern p = Pattern.compile(regex);
-	        Matcher m = p.matcher(RRN);
-
-	        if (m.find()) {
-	            System.out.println("올바른 형식입니다.");
-	        } else {
-	            System.out.println("올바르지 않은 형식입니다.");
-	        }
-		
+		if(Pattern.matches(regex, birth)) {
+			System.out.println("생년월일 : " + birth);
+		}else {
+			System.out.println("올바른 형식이 아닙니다.");
+		}
 	}
 }
+
+
+		  
+
